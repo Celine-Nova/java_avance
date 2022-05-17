ef7dad3a2b72f8dc1531c725cb6b20539085a06d
unknown <cocotte243@gmail.com>
2022-05-18 00:25:30 +0200

    condition while sur lla donnée d'eront©e de l'ilisateur

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args)
     {
-        // TP1 calcul surface
-        TP5.equation();
+
+        TP6.calendrier();
 
     }
 }
--- src/TP6.java
+++ src/TP6.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
 
-public class TP5 {
-
-    static void equation(){
+public class TP6 {
+    static byte number;
+    static void calendrier(){
 
         Scanner scanner = new Scanner(System.in);
-
-
+        System.out.println("Saisissez un nombre entre 1 et 12");
+        number = scanner.nextByte();
+        while(number < 1 || number > 12){
+            System.out.println("J'ai dit entre 1 et 12  Recommencez");
+            number = scanner.nextByte();
+        }
     }
 }
