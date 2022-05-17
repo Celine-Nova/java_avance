266830bbf2f4a1ebc57bff2b9191903e07de21a1
unknown <cocotte243@gmail.com>
2022-05-17 14:40:55 +0200

    equation second degré et discriminant

--- out/production/exo-java-avance/README.md
+++ out/production/exo-java-avance/README.md
@@ -15,3 +15,19 @@ Entrez la troisième valeur(c) : 235
 Les valeurs entrées sont : a = 51, b = 876 et c = 235
 Permutation: b <== a, c <== b, a <== c
 Les valeurs permutées sont : a = 235, b = 51 et c = 876
+
+**TP 4**
+Ecrivez un programme Java qui lit un nombre et indique s'il est positif, négatif ou s'il vaut zéro et s'il est pair ou impair.
+Exemple d'exécution:
+Entrez un nombre entier: 5
+Le nombre est positif et impair
+Entrez un nombre entier: -4
+Le nombre est négatif et pair
+Entrez un nombre entier: 0
+Le nombre est zéro (et il est pair)
+
+**TP 5**
+Ecrivez un programme Java qui permet de résoudre une équation du 2nd degré de la forme ax2+bx+c = 0.
+L'utilisateur devra fournir a,b,c à partir du clavier, ensuite le programme lui donnera la solution
+Principe du fonctionnement d'une équation du 2nd degré:
+https://www.maths-et-tiques.fr/telech/Secondegre2ESL.pdf
--- src/TP5.java
+++ src/TP5.java
@@ -1,10 +1,41 @@
-/*
-public class TP4 {
-    //A FAIRE
-    static void (){
+import java.util.Scanner;
 
+public class TP5 {
+    static double a;
+    static double b;
+    static double c;
 
-        System.out.println("");
+    static double x;
+    static void equation(){
+
+        Scanner scanner = new Scanner(System.in);
+        //Demander 3 nombres
+        System.out.println("Veuillez entre le 1er nombre :");
+        a = scanner.nextDouble();
+        System.out.println("Veuillez entre le 2eme nombre : ");
+        b = scanner.nextDouble();
+        System.out.println("Veuillez entre le 3eme nombre : ");
+        c = scanner.nextDouble();
+
+        // Equation du 2nd degré:
+        //ax 2+ bx + c = 0 === b² − 4 a c
+        String equation = "(ax 2+ bx + c = 0)";
+        double discriminant =  Math.pow(b,2 - 4 * a * c);
+      //  double delta= racineCarre - 4 * a * c ;
+        System.out.println("Le resultat de l'équation : " +  equation + " est : " + discriminant);
+        if(discriminant < 0 ){
+            System.out.println("L'équation n'a pas de solution réelle");
+        } else if (discriminant == 0) {
+            System.out.println("L'équation a une solution unique");
+            x = b/2 * a;
+            System.out.println("x = b / 2 * a" + x);
+        }else {
+            System.out.println("L'équation a deux solutions distinctes");
+            double x1 = -b - Math.sqrt(discriminant) / 2 * a;
+            System.out.println("Solution 1 : (x1=-b-(b²−4ac)/2*a) : " + x1);
+            double x2 = -b + Math.sqrt(discriminant) / 2 * a;
+            System.out.println("Solution 2 : (x2=-b+(b²−4ac)/2*a) : " + x2);
+        }
+
     }
 }
-*/
